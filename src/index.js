5c9329d0f619b8047d7f53171e30ea8516a31252
jelorivera08 <jerald@virtualcontrol.io>
2020-03-10 18:22:33 +0800

    include printSchema func

--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const cors = require('cors');
 const schema = require('./graphql/schema');
+const printSchemaFromBuild = require('./config/printSchema');
+
 
 const { setupDB } = require('./config/databaseConnection');
 
 const app = express();
 
-setupDB(v => console.log(v));
+setupDB((v) => console.log(v));
+app.use(cors());
 app.use(
   '/graphql',
   graphqlHTTP({
     schema,
     graphiql: true,
-    pretty: true
-  })
+    pretty: true,
+  }),
 );
 
 app.listen(4000);
 console.log('SERVER OK');
+printSchemaFromBuild(schema);
