409ffa309a5a29566b15228f118a3e84345eabd6
jelorivera08 <jerald@virtualcontrol.io>
2020-05-05 18:15:14 +0800

    address and AOD

--- src/graphql/mutation/UserMutation.js
+++ src/graphql/mutation/UserMutation.js
@@ -13,7 +13,8 @@ const UserCreate = {
     accountType: { type: new GraphQLNonNull(GraphQLString) },
     facebookURL: { type: new GraphQLNonNull(GraphQLString) },
     instagramURL: { type: new GraphQLNonNull(GraphQLString) },
-    description: { type: new GraphQLNonNull(GraphQLString) },
+    areaOfDistribution: { type: new GraphQLNonNull(GraphQLString) },
+    address: { type: new GraphQLNonNull(GraphQLString) },
     region: { type: new GraphQLNonNull(GraphQLString) },
   },
   resolve: async (_, values) => {
